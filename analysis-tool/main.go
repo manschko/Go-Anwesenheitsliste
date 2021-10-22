cb4595f421f09634d27b1120c159a69fb373832d
benedikt.hutter <benedikt.hutter@computer-creativ.com>
2021-10-22 10:45:44 +0200

    feat: Adding of code to search places, who a person be at on a day

--- analysis-tool/main.go
+++ analysis-tool/main.go
@@ -24,14 +24,27 @@ func ReadStdIn()(string, []string) {
   return command, parameter
 }
 
-func GetFileReader(day string) []byte {
-  reader, err := os.ReadFile("Journal/" + day)
+func GetFileContent(day string) string {
+  byteContent, err := os.ReadFile("Journal/" + day)
 
   if err != nil {
-    return nil
+    return "error"
   }
 
-  return reader
+  content := string(byteContent)
+
+  return content
+}
+
+func PrintHelp() {
+  fmt.Print("\thelp\t\t\t\tZeigt diese Hilfe an\n")
+  fmt.Print("\t\tselect-day 21-12-2012\t\tWählt ein Datum für weitere Befehle aus\n")
+  fmt.Print("\t\tsearch-person max.mustermann\tSucht die Orte an dem sich eine Person am Tag aufgehalten hat\n")
+  fmt.Print("\t\tlist-days\t\t\tZeigt die Tage an, an denen eine Anwesenheit protokolliert wurde\n")
+  fmt.Print("\t\texport-list Ort\t\t\tExportiert die Anwesenheitsliste für einen Ort in eine CSV-Datei\n")
+  fmt.Print("\t\texit\t\t\t\tBeendet dieses Programm")
+
+  fmt.Print("\n")
 }
 
 func ListDays() {
@@ -42,9 +55,10 @@ func ListDays() {
     return
   }
 
-  regexp := regexp.MustCompile("[0-9]*-[0-9]*-[0-9]*")
+  regexp := regexp.MustCompile("[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]")
+  fmt.Print("\tIndex\t| Tag\n")
 
-  for _, file := range files {
+  for i, file := range files {
     result := regexp.FindString(file.Name())
 
     if result == "" {
@@ -52,18 +66,45 @@ func ListDays() {
       continue
     }
 
-    fmt.Print("\t" + file.Name() + "\n")
+    fmt.Print("\t\t")
+    fmt.Print(i + 1)
+    fmt.Print("\t| " + file.Name() + "\n")
   }
 
   fmt.Print("\n")
 }
 
 func SearchPerson(parameter []string) {
+  content := GetFileContent(parameter[1])
+  rows := strings.Split(content, "\n")
+  var places []string
 
+  fmt.Print("\tGesuchter Name: " + parameter[0] + "\n")
+  fmt.Print("\t\tOrte:\n")
+  for _, row := range(rows) {
+    fields := strings.Split(row, ";")
+    if fields[0] != parameter[0] {
+      continue
+    }
+
+    containPlace := false
+    for _, place := range(places) {
+      if fields[1] == place {
+        containPlace = true
+      }
+    }
+
+    if !containPlace {
+      places = append(places, fields[1])
+      fmt.Print("\t\t- " + fields[1] + "\n")
+    }
+  }
 }
 
 func ExportList(parameter []string) {
-
+  //content := GetFileContent(parameter[1])
+  //rows := strings.Split(content, "\n")
+  //var places []string
 }
 
 func main() {
@@ -71,7 +112,7 @@ func main() {
   fmt.Print("\n### Go Anwesenheitsliste - Analyse Tool - begin ###\n\n")
 
   runCondition := true
-  // selectedDay := ""
+  selectedDay := ""
 
   for runCondition {
 
@@ -80,7 +121,11 @@ func main() {
     switch cmd {
 
       case "select-day":
-
+        if len(parameter) != 1 {
+          PrintHelp()
+          break
+        }
+        selectedDay = parameter[0]
         fmt.Print("\tDer " + parameter[0] + " wurde ausgewählt\n")
         break
 
@@ -89,10 +134,23 @@ func main() {
         break
 
       case "search-person":
+        if len(parameter) != 2 {
+          PrintHelp()
+          break
+        }
+
+        name := parameter[0] + " " + parameter[1]
+        parameter := []string{name, selectedDay}
         SearchPerson(parameter)
         break
 
       case "export-list":
+        if len(parameter) != 1 {
+          PrintHelp()
+          break
+        }
+
+        parameter = append(parameter, selectedDay)
         ExportList(parameter)
         break
 
@@ -101,20 +159,11 @@ func main() {
         runCondition = false
 
       default:
-        fmt.Print("\thelp\t\t\t\tZeigt diese Hilfe an\n")
-        fmt.Print("\t\tselect-day 21-12-2012\t\tWählt ein Datum für weitere Befehle aus\n")
-        fmt.Print("\t\tsearch-person max.mustermann\tSucht die Orte an dem sich eine Person am Tag aufgehalten hat\n")
-        fmt.Print("\t\tlist-days\t\t\tZeigt die Tage an, an denen eine Anwesenheit protokolliert wurde\n")
-        fmt.Print("\t\texport-list Ort\t\t\tExportiert die Anwesenheitsliste für einen Ort in eine CSV-Datei\n")
-        fmt.Print("\t\texit\t\t\t\tBeendet dieses Programm")
-
-        fmt.Print("\n")
+        PrintHelp()
     }
 
     fmt.Print("\n")
   }
-
-  //selectedDay = "wef"
 
   fmt.Print("### Go Anwesenheitsliste - Analyse Tool - end ###\n\n")
 }
