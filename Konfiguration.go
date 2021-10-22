2bf86b3bedff7787cbb69b6d023615d565d3f572
manschko <manschko1999@googlemail.com>
2021-10-22 12:36:40 +0200

    Setup webServer

--- Konfiguration.go
+++ Konfiguration.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net/http"
+	"sync"
 )
 //Struct für das Speichern der Flags
 type Flags struct {
@@ -19,6 +22,45 @@ func ConfigFlag() {
 
 	flag.Parse()
 
+}
+
+func ConfigWebServer(){
+	//WaitGroup für go routinen erstellt
+	wg := new(sync.WaitGroup)
+	//Setzte WaitGroup auf 2 für 2 go routinen
+	wg.Add(2)
+
+	// Setup für Anmeldeserver über go routine
+	go func() {
+		server := createServer("Login", flags.Port1)
+		fmt.Println( server.ListenAndServe())
+		wg.Done()
+	}()
+
+	// Setup für QRcode Seite über go routine
+	go func() {
+		server := createServer("QR", flags.Port2)
+		fmt.Println( server.ListenAndServe())
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func createServer(name string, port int)  *http.Server{
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func( res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "Hello: " + name)
+	})
+
+	server := http.Server {
+		Addr: fmt.Sprintf(":%v", port),
+		Handler: mux,
+	}
+
+	return &server
 }
 
 
