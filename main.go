792aa74abb3be21f9f5e37a3376729504e583c70
benedikt.hutter <benedikt.hutter@computer-creativ.com>
2021-10-23 15:19:21 +0200

    feat: Adding of basic stuff to build a QR code web page

--- main.go
+++ main.go
@@ -3,4 +3,13 @@ package main
 func main() {
 	ConfigFlag()
 	ConfigWebServer()
+
+	placeList, result := ReadLocationList()
+	if !result {
+		return
+	}
+
+	for _, location := range(placeList) {
+		
+	}
 }
