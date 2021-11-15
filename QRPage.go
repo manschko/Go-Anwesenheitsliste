ccb89fb0f891ea1e080db97fdbefc1a0ed8945a1
kPrager <Prager.Karin@gmail.com>
2021-11-15 19:06:47 +0100

    page qrCode

--- QRPage.go
+++ QRPage.go
@@ -15,7 +15,6 @@ type TemplateDataQR struct {
 func createQRWebServer(port int)  *http.Server{
 	mux := http.NewServeMux()
 
-	// Ursprüngliche Ausgabe Inhalt
 	mux.HandleFunc("/", func( res http.ResponseWriter, req *http.Request) {
 		//data := TemplateData{[]string{"test","test2"}}
 		path := filepath.FromSlash("./PageTemplates/qr.html")
@@ -25,9 +24,35 @@ func createQRWebServer(port int)  *http.Server{
 			return
 		}
 		//Todo if form got send
+		//getNewUrl(res, req)
+
 		fmt.Println(req.FormValue("location"))
 		//renderTemplate(res, req, "qr.html", TemplateData{[]string{}, true})
 	})
+
+	locations, check := ReadLocationList()
+	if check {
+		for _, location := range locations {
+
+			mux.HandleFunc("/" + location.Name, func( res http.ResponseWriter, req *http.Request) {
+				//data := TemplateData{[]string{"test","test2"}}
+				path := filepath.FromSlash("./PageTemplates/qrSingle.html")
+				tmpl := template.Must(template.ParseFiles(path))
+
+				fmt.Println(location.Name)
+
+				if req.Method != http.MethodPost{
+					tmpl.Execute(res, struct{locationName string}{location.Name})
+					return
+				}
+				//Todo if form got send
+
+				fmt.Println(req.FormValue("location"))
+				//renderTemplate(res, req, "qr.html", TemplateData{[]string{}, true})
+			})
+
+		}
+	}
 
 	server := http.Server {
 		Addr: fmt.Sprintf(":%v", port),
@@ -36,3 +61,4 @@ func createQRWebServer(port int)  *http.Server{
 
 	return &server
 }
+
