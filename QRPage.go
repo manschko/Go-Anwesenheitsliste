c312c027496478bc51e6b754040568aca07ce172
manschko <manschko1999@googlemail.com>
2021-12-20 17:27:22 +0100

    QRPage test implementation gitignore added

--- QRPage.go
+++ QRPage.go
@@ -14,59 +14,24 @@ type TemplateDataQR struct {
 	Locations []string
 	Success bool
 }
+var locations, check = ReadLocationList()
+var locationNameList []string
 
 func createQRWebServer(port int)  *http.Server{
 	m := mux.NewRouter()
-	locations, check := ReadLocationList()
+
 
 	// list of location names
-	var locationNameList []string
+
 	for _, location := range locations {
 		locationNameList = append(locationNameList, location.Name)
 	}
 
 	//show form with locations for selection
-	m.HandleFunc("/", func( res http.ResponseWriter, req *http.Request) {
-		path := filepath.FromSlash("./PageTemplates/qr.html")
-		tmpl := template.Must(template.ParseFiles(path))
-		if req.Method != http.MethodPost{
-			tmpl.Execute(res, TemplateDataQR{locationNameList,false})
-			return
-		}
-		//TODO eventuell abfangen wenn falsches ausgewählt wird das nicht gibt
-		//get selected location
-		selectedLocation := req.FormValue("location")
-
-		for _, location := range locations {
-			fmt.Println(location)
-			//check if selected location is in xml
-			if selectedLocation == location.Name {
-				//add accesstoken of selected location to url
-				http.Redirect(res, req, "/" + location.AccessToken, http.StatusSeeOther)
-			}
-		}
-	})
+	m.HandleFunc("/", selectionPageHandler)
 	if check {
 		//get location as param from url
-		m.HandleFunc("/{location}", func( res http.ResponseWriter, req *http.Request) {
-			params := mux.Vars(req)
-			for _, location := range locations {
-				//check if param is in xml
-				if params["location"] == location.AccessToken {
-					//load new template for qr code
-					path := filepath.FromSlash("./PageTemplates/qrSingle.html")
-					tmpl := template.Must(template.ParseFiles(path))
-					//generate qr code
-					executeQr(location)
-					tmpl.Execute(res, struct{LocationName string
-											Valid int}{location.Name, flags.TokenValidity})
-					return
-				}
-			}
-			// if param is not in xml redirect back to selection page
-			http.Redirect(res, req, "/", http.StatusSeeOther)
-			return
-		})
+		m.HandleFunc("/{location}", qrPageHandler)
 	}
 	//fileserver for gr code single page
 	fs := http.FileServer( http.Dir("./PageTemplates"))
@@ -81,4 +46,42 @@ func createQRWebServer(port int)  *http.Server{
 func executeQr(location Location) {
 
 	qrcode.WriteFile("https://" + flags.Url + ":" + strconv.Itoa(flags.Port1) + "/?location=" + location.AccessToken + "&access=" + location.CurrentToken, qrcode.Medium, 256, "PageTemplates/qr.png")
+}
+func qrPageHandler( res http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, location := range locations {
+		//check if param is in xml
+		if params["location"] == location.AccessToken {
+			//load new template for qr code
+			path := filepath.FromSlash("./PageTemplates/qrSingle.html")
+			tmpl := template.Must(template.ParseFiles(path))
+			//generate qr code
+			executeQr(location)
+			tmpl.Execute(res, struct{LocationName string
+				Valid int}{location.Name, flags.TokenValidity})
+			return
+		}
+	}
+	// if param is not in xml redirect back to selection page
+	http.Redirect(res, req, "/", http.StatusSeeOther)
+	return
+}
+
+func selectionPageHandler( res http.ResponseWriter, req *http.Request) {
+	path := filepath.FromSlash("./PageTemplates/qr.html")
+	tmpl := template.Must(template.ParseFiles(path))
+	if req.Method != http.MethodPost{
+		tmpl.Execute(res, TemplateDataQR{locationNameList,false})
+		return
+	}
+	//get selected location
+	selectedLocation := req.FormValue("location")
+
+	for _, location := range locations {
+		//check if selected location is in xml
+		if selectedLocation == location.Name {
+			//add accesstoken of selected location to url
+			http.Redirect(res, req, "/" + location.AccessToken, http.StatusSeeOther)
+		}
+	}
 }
---
+++ QRPage_test.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+
+
+func TestQRSelectionPage(t *testing.T) {
+	locations, _ := ReadLocationList()
+	res := httptest.NewRecorder()
+	//test if webpage is reachable
+	req, err := http.NewRequest("GET", "https://" + flags.Url + ":" + strconv.Itoa(flags.Port2) + "/", nil)
+	selectionPageHandler(res, req)
+
+
+	if err != nil{
+		t.Fatal(err)
+
+	}
+
+	data := url.Values{}
+	data.Set("location", locations[0].Name)
+	req, err = http.NewRequest("POST", "https://" + flags.Url + ":" + strconv.Itoa(flags.Port2) + "/", strings.NewReader(data.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	response, err := client.Do(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if(response.StatusCode != 200 || response.Request.URL.String() == "https://" + flags.Url + ":" + strconv.Itoa(flags.Port2) + "/" ) {
+		t.Errorf("Redirect to QR Page did not work")
+	}
+
+}
+func TestQRPage(t *testing.T) {
+	//check of redirect with wrong parameters
+	locations, _ := ReadLocationList()
+	req, err := http.NewRequest("GET", "https://" + flags.Url + ":" + strconv.Itoa(flags.Port2) + "/test" , nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	response, err := client.Do(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if(response.StatusCode != 200 || response.Request.URL.String() != "https://" + flags.Url + ":" + strconv.Itoa(flags.Port2) + "/" ) {
+		t.Errorf("Redirect with wrong Accescode did not work")
+	}
+
+	//check if QR code is getting generated
+	os.Remove("PageTemplates/qr.png")
+
+	req, err = http.NewRequest("GET", "https://" + flags.Url + ":" + strconv.Itoa(flags.Port2) + "/" + locations[0].AccessToken , nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response, err = client.Do(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println()
+
+	_, err = os.Stat("PageTemplates/qr.png"); errors.Is(err, os.ErrNotExist)
+	if err != nil {
+		t.Errorf("QR Code was not properly generated")
+	}
+}
+
+
